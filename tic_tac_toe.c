5af698c83010974f737e2fe4d8ae9bc4ca33d967
Omkar-1505 <omkarpadheetheboss@gmail.com>
2025-05-10 19:25:29 +0530

    working on tic tac toe game

---
+++ tic_tac_toe.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int check( int r[2][2]){
+    
+}
+void main(){
+    printf(" HELLO JI! \n WELCOME TO OUR TIC TAC TOE GAME!!! \n");
+    int r[2][2];
+    char x= 'x';
+    char o = 'o';
+    int k = check();
+
+}

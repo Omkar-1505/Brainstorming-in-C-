a7eb988d73b37ea86aa317ee0cda2e219ea7255f
Omkar-1505 <omkarpadheetheboss@gmail.com>
2025-05-10 22:25:06 +0530

    added a check function to analyse

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -1,5 +1,33 @@
 #include<stdio.h>
 int check( int r[2][2]){
+    int s;
+    for(int c=0; c<3 ; c++){
+        char k = r[c][0];
+        char p = r[c][1];
+        char z = r[c][2];
+        if( k==p && p==z && k==z){
+            s=1;
+            break;
+        }
+        else{
+            s=0;
+        }
+    }
+    for(int d=0; d<3 ; d++){
+        char k = r[0][d];
+        char p = r[1][d];
+        char z = r[2][d];
+        if( k==p && p==z && k==z){
+            s=1;
+            break;
+        }
+        else{
+            s=0;
+        }
+    }
+    
+
+
     
 }
 void main(){
